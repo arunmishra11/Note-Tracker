d854e531a080def040c78a34428e68c89f3c1cc3
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-05-04 12:11:41 -0500

    installed uuid module

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const { v4: uuidv4 } = require("uuid"); // Import uuidv4 from uuid module
 const fs = require("fs");
 
 // Read JSON data from file
@@ -11,6 +12,8 @@ function readJSONFile(filePath) {
 const writeJSONFile = (filePath, jsonData) => {
   fs.writeFileSync(filePath, JSON.stringify(jsonData));
 };
+
+const router = express.Router();
 
 // GET request handler for '/api/notes' endpoint
 router.get("/api/notes", (req, res) => {
@@ -28,7 +31,7 @@ router.post("/api/notes", (req, res) => {
   const newNote = {
     title: req.body.title,
     text: req.body.text,
-    id: uuidv4(),
+    id: uuidv4(), // Use uuidv4 function to generate a unique ID
   };
   // Add the new note to the JSON data
   dbJson.push(newNote);
@@ -39,11 +42,11 @@ router.post("/api/notes", (req, res) => {
 });
 
 // DELETE request handler for '/api/notes/:id' endpoint
-router.delete('/api/notes/:id', (req, res) => {
+router.delete("/api/notes/:id", (req, res) => {
   // Read data from the JSON file
   let dbJson = readJSONFile("db/db.json");
   // Filter out the note with the specified ID
-  dbJson = dbJson.filter(note => note.id !== req.params.id);
+  dbJson = dbJson.filter((note) => note.id !== req.params.id);
   // Write updated data back to the JSON file
   writeJSONFile("db/db.json", dbJson);
   // Send confirmation message as response
