1e47fbc263d9c452553a9d498df9bb7bb3f195b0
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-05-04 10:17:00 -0500

    update variables to be camelCase and added comments for readbility

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 
 // Importing custom HTML routes module
-const html_routes = require('./routes/html-routes')
+const htmlRoutes = require('./routes/html-routes');
 
 // Importing custom API routes module
-const api_routes = require('./routes/api-routes')
+const apiRoutes = require('./routes/api-routes');
 
 // Setting the port for the server to listen on, using environment variable if available, otherwise using port 3001
 const PORT = process.env.PORT || 3001;
@@ -13,20 +13,22 @@ const PORT = process.env.PORT || 3001;
 // Creating an instance of the Express application
 const app = express();
 
-// Express middleware to parse url-encoded data, setting 'extended' option to false for querystring parsing
+// Middleware
+
+// Parsing url-encoded data
 app.use(express.urlencoded({ extended: false }));
 
-// Express middleware to parse JSON data
+// Parsing JSON data
 app.use(express.json());
 
-// Express middleware to serve static files such as images, CSS, JavaScript files, etc. from the 'public' directory
+// Serving static files such as images, CSS, JavaScript files, etc. from the 'public' directory
 app.use(express.static("public"));
 
 // Mounting the HTML routes middleware
-app.use(html_routes)
+app.use(htmlRoutes);
 
 // Mounting the API routes middleware
-app.use(api_routes)
+app.use(apiRoutes);
 
 // Start the Express server and make it listen on the specified port
 app.listen(PORT, () => {
